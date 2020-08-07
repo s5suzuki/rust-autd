0f627ec7d2aa4dc8caed66f86ab89d60172fe406
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-08-07 15:26:48 +0900

    fix: example/twincat for nix

--- autd-examples/src/twincat.rs
+++ autd-examples/src/twincat.rs
@@ -4,7 +4,7 @@
  * Created Date: 25/05/2020
  * Author: Shun Suzuki
  * -----
- * Last Modified: 25/05/2020
+ * Last Modified: 07/08/2020
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2020 Hapis Lab. All rights reserved.
@@ -14,9 +14,13 @@
 mod tests;
 
 use autd::prelude::*;
+#[cfg(target_os = "windows")]
 use autd_twincat_link::LocalTwinCATLink;
+#[cfg(not(target_os = "windows"))]
+use autd_twincat_link::RemoteTwinCATLink;
 use tests::*;
 
+#[cfg(target_os = "windows")]
 fn main() {
     let mut autd = AUTD::create();
 
@@ -25,6 +29,28 @@ fn main() {
 
     let link = LocalTwinCATLink::new();
 
+    autd.open(link).expect("Failed to open");
+
+    run(autd);
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {
+    use std::io;
+    use std::io::Write;
+
+    let mut autd = AUTD::create();
+
+    autd.geometry()
+        .add_device(Vector3::zeros(), Vector3::zeros());
+
+    print!("Enter a remote TwinCAT AUTD Server address: ");
+    io::stdout().flush().unwrap();
+
+    let mut addr = String::new();
+    io::stdin().read_line(&mut addr).unwrap();
+
+    let link = RemoteTwinCATLink::new(&addr);
     autd.open(link).expect("Failed to open");
 
     run(autd);
